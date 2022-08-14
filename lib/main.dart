45a3483aca160b5aa06e4209b064adee494acd67
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-08-15 01:10:01 +0530

    string interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import './transaction.dart';
 import 'package:flutter/material.dart';
 
@@ -53,14 +55,44 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                     children: <Widget>[
                       Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
                         child: Text(
-                          tx.amount.toString(),
+                         '₹ : ${tx.amount}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
                         ),
                       ),
-                      Column(children: <Widget>[
-                        Text(tx.title),
-                        Text(tx.date.toString(),),
-                      ],)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(
+                             color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      )
                     ],
                   ),
                 );
